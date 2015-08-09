80de8d3bebeec1b58ae4af5758ca13cae9c962a5
Chun-Hung Tseng <henrybear327@gmail.com>
2015-08-09 09:42:09 +0800

    Add template, vimrc , and bashrc for Contest

---
+++ templates/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <climit> //LLONG_MIN LLONG_MAX INT_MIN INT_MAX
+
+#define MIN(x,y) ((x) < (y) ? (x) : (y)
+#define MAX(x,y) ((x) > (y) ? (x) : (y)
+
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
