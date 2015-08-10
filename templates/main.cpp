5546cf697c704b67ab05696e8da5f5010f75994e
Chun-Hung Tseng <henrybear327@gmail.com>
2015-08-11 00:21:09 +0800

    Fix a include typo

--- templates/main.cpp
+++ templates/main.cpp
@@ -5,16 +5,14 @@
 #include <stack>
 #include <queue>
 #include <vector>
-#include <climit> //LLONG_MIN LLONG_MAX INT_MIN INT_MAX
+#include <climits> //LLONG_MIN LLONG_MAX INT_MIN INT_MAX
 
-#define MIN(x,y) ((x) < (y) ? (x) : (y)
-#define MAX(x,y) ((x) > (y) ? (x) : (y)
+#define MIN(x, y) ((x) < (y) ? (x) : (y)
+#define MAX(x, y) ((x) > (y) ? (x) : (y)
 
 using namespace std;
 
 int main()
 {
-
-
     return 0;
 }
