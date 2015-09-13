d1f398481659e720faaf5f5178e89a543428e6ea
Chun-Hung Tseng <henrybear327@gmail.com>
2015-09-13 10:43:26 +0800

    Add my own implementation of D and H, remove Codeblocks project files

--- Gym/2013-2014 Samara SAU ACM ICPC Quarterfinal Qualification Contest/H/main.cpp
+++ Gym/2013-2014 Samara SAU ACM ICPC Quarterfinal Qualification Contest/H/main.cpp
@@ -1,39 +1,31 @@
 #include <cstdio>
-#include <cstring>
-#include <cmath>
-#include <iostream>
-#include <algorithm>
-using namespace std;
 
 int main()
 {
-    int N;
-    while(scanf("%d",&N)!=EOF) {
-        N+=2;
-        char a[N];
-        char b[N];
+    int len;
+    while (scanf("%d", &len) == 1) {
+        char input[len + 10], input2[len + 10]; //be aware of the overflow problem!!
+        fgets(input, len + 10, stdin);
 
-        memset(a,'\0',sizeof(a));
-        memset(b,'\0',sizeof(b));
-        fgets(a,N,stdin);
-        fgets(a,N,stdin);
-        fgets(b,N,stdin);
+        fgets(input, len + 10, stdin);
+        fgets(input2, len + 10, stdin);
 
-        N-=2;
-
-        int cnt=0;
-        int flag = 0;
-        for(int i=0; i<N; i++) {
-            if(a[i]==b[i]) {
-                if(flag) {
-                    flag = 0;
+        bool consecutive = false;
+        int cnt = 0;
+        for (int i = 0; i < len; i++) {
+            if (input[i] != input2[i]) {
+                if (!consecutive) {
+                    consecutive = !consecutive;
+                    cnt++;
                 }
-            } else if(a[i]!=b[i] && !flag) {
-                flag = 1;
-                cnt++;
+            } else {
+                if (consecutive)
+                    consecutive = !consecutive;
             }
         }
-        printf("%d\n",cnt);
+
+        printf("%d\n", cnt);
     }
+
     return 0;
 }
