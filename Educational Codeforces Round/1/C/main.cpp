20746ed8aa07de2a02ac65d267ffe5caf0ad432c
Chun-Hung Tseng <henrybear327@gmail.com>
2015-11-15 03:15:38 +0800

    Finally got C to AC

--- Educational Codeforces Round/1/C/main.cpp
+++ Educational Codeforces Round/1/C/main.cpp
@@ -1,29 +1,18 @@
 #include <bits/stdc++.h>
 // LLONG_MIN LLONG_MAX INT_MIN INT_MAX
 
-#define PI (atan(1) * 4)
+// const long double pi = M_PI;
+#define pi atan(1) * 4
 
 using namespace std;
 
 #define INF 1e9
 
-inline double atan2_process(double x, double y)
-{
-    double ans = atan2(y, x);
-    if (ans < 0.0)
-        ans += 2 * PI;
-
-    return ans;
-}
-
-inline double get_small_angle(double x)
-{
-    return x > PI ? 2 * PI - x : x;
-}
+#define DEBUG 0
 
 typedef struct data {
     int x, y;
-    double s;
+    long double s;
     int idx;
 } Data;
 
@@ -40,29 +29,42 @@ int main()
     Data inp[n];
     for (int i = 0; i < n; i++) {
         scanf("%d %d", &inp[i].x, &inp[i].y);
-        inp[i].s = atan2_process(inp[i].x, inp[i].y);
+        inp[i].s = atan2(inp[i].y, inp[i].x) + pi;
         inp[i].idx = i + 1;
     }
 
     sort(inp, inp + n, cmp);
 
-    /*
+#if DEBUG
     for (int i = 0; i < n; i++)
-        printf("%d %d %f %d\n", inp[i].x, inp[i].y, inp[i].s, inp[i].idx);
-    */
+        printf("%d %d %.15f %d\n", inp[i].x, inp[i].y, inp[i].s, inp[i].idx);
+#endif
 
-    double ans = INF;
+    long double ans = 1e9;
     int ans1 = -1, ans2 = -1;
+
     for (int i = 1; i < n; i++) {
-        if (get_small_angle(inp[i].s - inp[i - 1].s) < ans) {
-            ans = get_small_angle(inp[i].s - inp[i - 1].s);
-            ans1 = inp[i - 1].idx;
-            ans2 = inp[i].idx;
+        long double diff = inp[i].s - inp[i - 1].s;
+        diff = min(diff, 2 * pi - diff);
+#if DEBUG
+        printf("%.15Lf %.15Lf\n", diff, ans);
+#endif
+
+        if (diff < ans) {
+            ans = diff;
+            ans1 = inp[i].idx;
+            ans2 = inp[i - 1].idx;
+#if DEBUG
+            printf("%d %d\n", ans1, ans2);
+#endif
         }
     }
 
-    if (get_small_angle(inp[n - 1].s - inp[0].s) < ans)
-        printf("%d %d\n", inp[n - 1].idx, inp[0].idx);
+    long double diff = inp[n - 1].s - inp[0].s;
+    diff = min(diff, 2 * pi - diff);
+
+    if (diff < ans)
+        printf("%d %d\n", inp[0].idx, inp[n - 1].idx);
     else
         printf("%d %d\n", ans1, ans2);
 
