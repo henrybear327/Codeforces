94f8a35caae784441170fae3067126830d1883d3
Chun-Hung Tseng <henrybear327@gmail.com>
2016-02-20 21:12:50 +1100

    Add C and D

--- Div 2/8VC Venture Cup 2016 - Elimination Round/C/Main.java
+++ Div 2/8VC Venture Cup 2016 - Elimination Round/C/Main.java
@@ -1,91 +1,120 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        MyScanner sc = new MyScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
+public class Main {
+	public static long gcd(long a, long b) {
+		return b == 0 ? a : gcd(b, a % b);
+	}
 
-        // Start writing your solution here.
+	public static void main(String[] args) {
+		MyScanner sc = new MyScanner();
+		out = new PrintWriter(new BufferedOutputStream(System.out));
 
-        /*
-        int n      = sc.nextInt();        // read input as integer
-        long k     = sc.nextLong();       // read input as long
-        double d   = sc.nextDouble();     // read input as double
-        String str = sc.next();           // read input as String
-        String s   = sc.nextLine();       // read whole line as String
+		// while (true) {
+		/*
+		 * // WA // long a = sc.nextInt(), b = sc.nextInt(); long a = (long)
+		 * (Math.random() * 100) % 10 + 1, b = (long) (Math.random() * 100) % 10
+		 * + 1;
+		 * 
+		 * long i = 1, x = 2 * a, y = 3 * b; while (6 * i <= Math.min(x, y))
+		 * i++; i--;
+		 * 
+		 * while (i > 0) { boolean xi; if (x > y) { xi = false; y += 3; } else {
+		 * xi = true; x += 2; }
+		 * 
+		 * if (x == y) // 6 3 will get you wrong // changing to x += 2 --> 10, 9
+		 * will get you wrong, because you don'y know if // the multiple of 6 is
+		 * used or not if (xi == true) x += 2; else y += 3;
+		 * 
+		 * i--; // out.printf("%d %d\n", x, y); }
+		 * 
+		 * out.println(Math.max(x, y));
+		 * 
+		 * // end WA 5
+		 */
 
-    	while(sc.hasNext()) {
-    		...
-    	}
+		// linear search for answer
+		/*
+		 * int n = sc.nextInt(), m = sc.nextInt();
+		 * 
+		 * int i = 1; while(!(n <= i / 2 && m <= i / 3 && n + m <= i / 2 + i / 3
+		 * - i / 6)) i++; out.println(i);
+		 */
 
-        int result = 3*n;
-        out.println(result);                    // print via PrintWriter
-        */
+		// Binary search for answer
+		int n = sc.nextInt(), m = sc.nextInt();
+		// int n = (int) a, m = (int) b;
 
-        // Stop writing your solution here.
-        out.close();
-    }
+		int left = 1, right = 6 * Math.max(n, m);
+		while (right - left > 1) {
+			int mid = (left + right) / 2;
 
-    // PrintWriter for faster output
-    public static PrintWriter out;
+			if (n <= mid / 2 && m <= mid / 3 && n + m <= mid / 2 + mid / 3 - mid / 6)
+				right = mid;
+			else
+				left = mid;
+		}
 
-    // MyScanner class for faster input
-    public static class MyScanner
-    {
-        BufferedReader br;
-        StringTokenizer st;
+		out.println(right);
+		// left is WA and right is AC because left only holds the ELSE case
 
-        public MyScanner()
-        {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		/*
+		 * if (Math.max(x, y) != right) { out.println("Error: " + a + ' ' + b);
+		 * break; } else out.println("Ok: " + a + ' ' + b);
+		 */
+		// }
+		out.close();
+	}
 
-        boolean hasNext()
-        {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (Exception e) {
-                    return false;
-                }
-            }
-            return true;
-        }
+	// PrintWriter for faster output
+	public static PrintWriter out;
 
-        String next()
-        {
-            if (hasNext())
-                return st.nextToken();
-            return null;
-        }
+	// MyScanner class for faster input
+	public static class MyScanner {
+		BufferedReader br;
+		StringTokenizer st;
 
-        int nextInt()
-        {
-            return Integer.parseInt(next());
-        }
+		public MyScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        long nextLong()
-        {
-            return Long.parseLong(next());
-        }
+		boolean hasNext() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					return false;
+				}
+			}
+			return true;
+		}
 
-        double nextDouble()
-        {
-            return Double.parseDouble(next());
-        }
+		String next() {
+			if (hasNext())
+				return st.nextToken();
+			return null;
+		}
 
-        String nextLine()
-        {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
 }
--- Div 2/8VC Venture Cup 2016 - Elimination Round/D/main.cpp
+++ Div 2/8VC Venture Cup 2016 - Elimination Round/D/main.cpp
@@ -13,5 +13,31 @@ using namespace std;
 
 int main()
 {
+    ll n; //Crucial bug, be neat!
+    scanf("%lld", &n);
+
+    int inp[n];
+    for(int i = 0; i < n; i++)
+	scanf("%d", &inp[i]);
+
+    // get all combanation's diff cnt
+    ll diff[5010] = {0};
+    for(int i = 0; i < n; i++)
+	for(int j = i + 1; j < n; j++)
+	    diff[abs(inp[i] - inp[j])]++;
+
+    ll prefix[5010] = {0}; // get the cnt prefix sum from the end
+    for(int i = 5000; i >= 0; i--)
+	prefix[i] = prefix[i + 1] + diff[i];
+
+    ll ans = 0;
+    for(int i = 0; i <= 5000; i++)
+	for(int j = 0; j <= 5000; j++)
+	    if(i + j + 1 <= 5000) // crucial bug!! Be neat!
+		ans += diff[i] * diff[j] * prefix[i + j + 1];
+
+    n = n * (n - 1) / 2;// c(n, 2)
+    printf("%.15f\n", (double) ans / (n * n * n)); 
+
     return 0;
 }
