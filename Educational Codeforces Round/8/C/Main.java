628721d23c754dddf93082f92b2c2adbb49c6a53
Chun-Hung Tseng <henrybear327@gmail.com>
2016-02-21 23:20:22 +1100

    Use new string instead of StringBuilder

--- Educational Codeforces Round/8/C/Main.java
+++ Educational Codeforces Round/8/C/Main.java
@@ -31,12 +31,15 @@ public class Main {
 			/*
 			 * for (int i = 0; i < n; i++) out.printf("%c", ans[i]);
 			 */
-
+			
+			/*
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < n; i++)
 				sb.append(ans[i]);
 			sb.append('\n');
 			out.println(sb);
+			*/
+			out.println(new String(ans));
 		}
 
 		out.close();
