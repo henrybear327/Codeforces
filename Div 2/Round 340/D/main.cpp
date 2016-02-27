6579b784e184c5c3ada4f542ee5ccd8668922318
Chun-Hung Tseng <henrybear327@gmail.com>
2016-02-27 21:39:28 +1100

    Add Round 340 D

--- Div 2/Round 340/D/main.cpp
+++ Div 2/Round 340/D/main.cpp
@@ -1,47 +1,59 @@
 #include <bits/stdc++.h>
+// solution 2: ans == 2 when all dots are on the corners of a rectangle!
 
 using namespace std;
 
 int main()
 {
     pair<int, int> inp[3];
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d", &inp[i].first, &inp[i].second);
-    }
-
+    for(int i = 0; i < 3; i++)
+	scanf("%d %d", &inp[i].first, &inp[i].second);
+    
     sort(inp, inp + 3);
+	
+    int a = inp[0].first;
+    int b = inp[1].first;
+    int c = inp[2].first;
+    int d = inp[0].second;
+    int e = inp[1].second;
+    int f = inp[2].second;
 
-    // special case one line
-    bool ok = false;
-    for (int i = 0; i < 1; i++) {
-        if ((inp[i].first == inp[i + 1].first) &&
-            (inp[i + 1].first == inp[i + 2].first))
-            ok = true;
-        if ((inp[i].second == inp[i + 1].second) &&
-            (inp[i + 1].second == inp[i + 2].second))
-            ok = true;
+    int ans = -1;
+    // on same x/y axis
+    if(a == b && b == c)
+	ans = 1;
+    if(d == e && e == f)
+	ans = 1;
+    
+    // two dots in a line
+    if(ans == -1) {
+	if(a == b) {
+	    if(d < f && f < e)
+		ans = 3;
+	    else
+		ans = 2;
+	} else if(b == c) {
+	    if(e < d && d < f)
+		ans = 3;
+	    else
+		ans = 2;
+	} else if(d == e) {
+	    if(a < c && c < b)
+		ans = 3;
+	    else
+		ans = 2;
+	} else if(e == f) {
+	    if(b < a && a < c)
+		ans = 3;
+	    else
+		ans = 2;
+	} else {
+	    // else
+	    ans = 3;
+	}
     }
-
-    if (ok == true) {
-        printf("1\n");
-        return 0;
-    }
-
-    // first
-    int dx = inp[1].first - inp[0].first;
-    int dy = inp[1].second - inp[0].second;
-
-    int cnt = 0;
-    if (dx)
-        cnt++;
-    if (dy)
-        cnt++;
-
-    dy = inp[2].second - inp[1].second;
-    if (dy)
-        cnt++;
-
-    printf("%d\n", cnt);
+    
+    printf("%d\n", ans);
 
     return 0;
 }
